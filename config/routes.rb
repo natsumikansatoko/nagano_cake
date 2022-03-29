260935dd771a5ded36c44f39f37b863e5729c5e1
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-03-29 11:45:32 +0000

    [Add]ルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  
   devise_for :customers
   devise_for :admins
+  
+  namespace :admin do
+   
+  end
+  
+  root 'homes#top'
+  get '/about' => 'homes#about'
+  resources :items, only: [:index, :show]
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
