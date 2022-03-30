b0f45e760fcc765cc9bf0dc14865586dadb0a2dc
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-03-30 13:55:51 +0000

    [Add]管理者ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  
+
   devise_for :customers
+
   devise_for :admins
   
   namespace :admin do
-   
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_items, only: [:update]
   end
-  
-  root 'homes#top'
-  get '/about' => 'homes#about'
-  resources :items, only: [:index, :show]
-  
-  
-  
+
+  scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
