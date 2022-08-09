bfba1ae65e2126019bf772b7ffbd41d5a9ee5a1f
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-08-09 04:58:24 +0000

    [Add]コントローラー・モデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,71 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/create'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/delete'
+    get 'cart_items/delete_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdrawal'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
