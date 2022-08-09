c1ab285a5b82986f8e932c70c07587427f978c63
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-08-09 06:20:27 +0000

    [Update]route編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'order_details/update'
+    get "/" => "homes#top"
+    resources :imtes, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :order, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+
+
   namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
