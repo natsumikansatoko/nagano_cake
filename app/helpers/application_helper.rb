08a7bb754d049c478924a98f929bf638ae5da8d9
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-08-23 00:46:46 +0000

    [Add]注文履歴詳細画面追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sub_price(sub)
+    sub.item.with_tax_price * sub.amount
+  end
+
+  def total_price(totals)
+    price = 0
+    totals.each do |total|
+      price += sub_price(total)
+    end
+    return price
+  end
 end
