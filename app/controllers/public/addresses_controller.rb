e8f3882001937fd9d6d0a41055722de754d44217
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-09-04 03:02:21 +0000

    [Add]配送先登録/一覧画面追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,44 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    @address = current_customer.address
+    redirect_to addresses_path
   end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :addresss)
+  end
+
 end
+
