e4eefa72123bbaf1556181f9985c7c3023eb8d0c
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-09-04 02:15:57 +0000

    [Add]会員情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,11 +8,28 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdrawal
+    @customer = current_customer
+    @customer.withdrawal_status = true
+    if @customer.save
+      reset_session
+      redirect_to root_path
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 end
