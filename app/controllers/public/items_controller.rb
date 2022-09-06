ff2246d7a8df67943b30f9f1c8f66039a50c0bb6
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-09-06 07:01:01 +0000

    [Add]商品一覧

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,16 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(8)
+    @items_all = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image_id)
   end
 end
